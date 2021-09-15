50e2bb1c991cceb8b1e4e6d90552aed9e6d4daa0
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-09-15 11:10:12 +0200

    started on routes, get all data and get one id, still need work on error code

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const PORT = 5555;
+const hamsterRouter = require('./routes/hamsters.js')
+
+
+app.use( express.urlencoded({ extended: true }) )
+app.use( express.json() )
+app.use((req, res, next) => {
+	console.log(`${req.method}  ${req.url}`, req.body);
+	next()
+})
+
+//routes/endpoints
+
+app.use('/hamsters', hamsterRouter)
+
+app.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}`)
+})
