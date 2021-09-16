7d54408712c7cfc28b866eb5164229abaa80eab4
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-09-16 16:25:42 +0200

    fått ut vinnare i array, dock behövs mer jobb på "cutest"

--- src/routes/hamsters.js
+++ src/routes/hamsters.js
@@ -24,6 +24,11 @@ router.get('/random', async (req, res) =>{ //GET random
     res.send(randomHamster)
 })
 
+router.get('/cutest', async (req, res) =>{
+    let cutestArray = await getCutest()
+    res.send(cutestArray)
+})
+
 router.get('/:id', async (req, res)=>{ //Id
     const idUser = await getOneHamster(req.params.id) //får tillbaka id från funktionen
     if(!idUser){
@@ -47,12 +52,13 @@ router.get('/:id', async (req, res)=>{ //Id
      
 })
 
-router.delete('/:id', async (req, res) =>{
+router.delete('/:id', async (req, res) =>{ //deletear men får statuskod 400 ändå
     let deleted = await deleteOne(req.params.id)
     if(!deleted ){
         res.sendStatus(400)
     }
-    res.sendStatus(200)
+    res.send()
+    return
 })
 
 
@@ -63,7 +69,7 @@ async function HamsterObject(id){
     console.log('updating one document')
     
      const UpdateData = {
-        age: 3
+        wins: 3
      }
 
     const docRef = db.collection(HAMSTERS).doc(id) // hämta databasobjektet med id från parameter
@@ -108,7 +114,7 @@ async function getOneHamster(id) {
     }
 }
 
-async function deleteOne(id) {
+async function deleteOne(id) { //får error, vrf??
 
 	const docRef = db.collection(HAMSTERS).doc(id)
 	const docSnapshot = await docRef.get()
@@ -121,11 +127,22 @@ async function deleteOne(id) {
 	
 }
 
- async function updateHamster(id, object){
-     const docRef = db.collection(HAMSTERS).doc(id)
-     docRef.set(object)
+async function getCutest() {
+    const array = await getHamsters()
+
+    if(array.empty){
+        return []
+    }
+        let cutest = [];
+        for (let i = 0; i < array.length; i++){ //loopa igenom array, ta ut obj till min array
+        cutest[i] = array[i].wins; //returns all objects w array[i], .wins returns number of wins
+    }
+    
+    let finalResult = Math.max(...cutest) // får ut högsta talet här på wins.
+    console.log(finalResult)
+
+    //Leta sen upp vilken obj i array som matchar med resultatet på wins??
+     return 
 }
-
-
 
 module.exports = router
